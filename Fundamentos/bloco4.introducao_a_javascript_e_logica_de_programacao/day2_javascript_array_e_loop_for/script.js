6676dc0a38f94b5264a4abee6634e6aafba8392a
calilisantos <calilisantos@gmail.com>
2021-11-08 18:33:36 -0300

    Exercicio em construçao

---
+++ Fundamentos/bloco4.introducao_a_javascript_e_logica_de_programacao/day2_javascript_array_e_loop_for/script.js
@@ -0,0 +1,32 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//1.Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log()
+console.log(numbers)
+
+//2.Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
+let plusNumbers = [0]
+for( i = 1; i <numbers.length-1; i++) {
+  plusNumbers.push = plusNumbers[i]+numbers[i]
+}
+console.log(plusNumbers)
+
+//3.Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
+
+//4.Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+
+
+//5.Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
+
+//6.Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+
+
+//7.Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+
+
+//8.Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+
+
+//9.Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+
